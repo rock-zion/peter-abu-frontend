d2dc4182c9a5acfc0fc06de09d7e021e7b5f2887
rockxzion <rock_zion@outlook.com>
2023-07-17 00:55:06 +0100

    plugin dynamic data

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -7,6 +7,7 @@ import parse from 'html-react-parser';
 import style from './styles/portfolio-preview.module.scss';
 import { useAnimate, stagger } from 'framer-motion';
 import useIntersectionObserver from '@/lib/useIntersectionObserver';
+import Link from 'next/link';
 
 export default function Portfolio({ content }) {
   const portfolioParent = useRef(null);
@@ -20,7 +21,7 @@ export default function Portfolio({ content }) {
   return (
     <div ref={portfolioParent} className={style.project}>
       <div ref={scope} className={style.project_text_content}>
-        {parse(content.title)}
+        <h2 className='animate-portfolio-title'>{parse(content.title)}</h2>
         <div className='flex flex-wrap'>
           <div className={`${style.platform} animate-pebble sm:mb-4 `}>
             <Image
@@ -30,21 +31,29 @@ export default function Portfolio({ content }) {
           </div>
           {content.roles.map((x = {}, idx) => (
             <div
-              className={`${style.pebble} ${x.style} animate-pebble`}
+              className={`${style.pebble} ${
+                x.title === 'Sole Designer'
+                  ? 'bg-my-orange text-my-grey-0'
+                  : x.title === 'Co-Designer'
+                  ? 'bg-my-lemon text-my-grey-0'
+                  : 'bg-my-bg-color-1 text-my-grey-1'
+              } animate-pebble`}
               key={idx}>
               {x.title}
             </div>
           ))}
         </div>
-        <a className='case-study-link animate-portfolio-link'>
+        <Link
+          href='/project'
+          className='case-study-link animate-portfolio-link'>
           <span>Case Study</span> <Image alt='link icon' src={arrowRight} />
-        </a>
+        </Link>
       </div>
-      <div className={style.image_container}>
+      <div className={`${style.image_container} image-container`}>
         <Image
-          className={style.image}
+          className='image'
           fill={true}
-          src={content.image.url}
+          src={content.portfolioImage.image.data.attributes.formats}
           alt=''
         />
       </div>
