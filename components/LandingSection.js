cf7c672f8f0c2bdc6f5ecb432aaec870a26b4b23
rockxzion <rock_zion@outlook.com>
2023-07-17 00:55:29 +0100

    plugin dynamic data to landing section

--- components/LandingSection.js
+++ components/LandingSection.js
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import arrowDown from '../public/arrow-down.svg';
 import useStaggerAnimation from '@/lib/useStaggerAnimation';
 
-export default function LandingSection() {
+export default function LandingSection({ data = {} }) {
+  const { landingTitle, landingSubTitle, landingButtonText } = data;
   const [animateIt] = useState(true);
   const scope = useStaggerAnimation(animateIt);
 
@@ -12,14 +13,12 @@ export default function LandingSection() {
     <section ref={scope} className={`${style.landing_section} scroll-area `}>
       <div className={`${style.main} main-content`}>
         <div>
-          <h1 className='text-my-grey-0 animate-stagger'>Peter Abu-Ekpeshie</h1>
-          <h2 className='text-my-grey-1 animate-stagger'>
-            Product designer living in London, United Kingdom.
-          </h2>
+          <h1 className='text-my-grey-0 animate-stagger'>{landingTitle}</h1>
+          <h2 className='text-my-grey-1 animate-stagger'>{landingSubTitle}</h2>
           <a
             href='#selected-work'
             className={`${style.button} animate-stagger`}>
-            <span> Selected Work</span>
+            <span>{landingButtonText}</span>
             <Image src={arrowDown} alt='' srcSet='' />
           </a>
         </div>
